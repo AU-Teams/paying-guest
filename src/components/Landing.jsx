4ccbf5d99cc6fb79d78a23a09509858dedc44bdc
Anil Kumar <ak1109539@gmail.com>
2025-07-23 16:40:26 +0530

    commit 3

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,7 +5,7 @@ function Landing() {
   return (
     <div className="landing-page">
       <section className="hero">
-        <img src="../src/assets/image3.jpeg" alt="image" />
+        <img src="../public/image3.jpeg" alt="image" />
         <div className='hero-info'>
           <h1>Welcome to the Tenant Management System</h1>
           <p>Manage your tenants effectively and efficiently.</p>
