6dfd72904a57c49716284cb4575b5858e2405aff
Anil Kumar <ak1109539@gmail.com>
2025-08-04 18:43:37 +0530

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,12 @@
 import './Landing.css';
 import React from 'react';
+import bgimage from '../assets/image3.jpeg'
 
 function Landing() {
   return (
     <div className="landing-page">
       <section className="hero">
-        <img src="../public/image3.jpeg" alt="image" />
+        <img src={bgimage} alt="image" />
         <div className='hero-info'>
           <h1>Welcome to the Tenant Management System</h1>
           <p>Manage your tenants effectively and efficiently.</p>
